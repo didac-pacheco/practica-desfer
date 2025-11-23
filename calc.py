324e5542b0080b2f90589e30391cea66985645f5
Dídac Pacheco Drago <dpacheco@insmollerussa.cat>
2025-11-23 17:53:41 +0100

    Solució error per desfer canvis

--- calc.py
+++ calc.py
@@ -1,8 +1,11 @@
 def suma(a, b):
    return a + b
+<<<<<<< HEAD
 
 def resta(a, b):
    return a - b
 
 def multiplica(a, b):
    return a * b
+=======
+>>>>>>> parent of b19ed57 (B: afegir funció resta)
