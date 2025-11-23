2cfb77ac480a00efc6838872692a643036a58574
Dídac Pacheco Drago <dpacheco@insmollerussa.cat>
2025-11-23 18:00:58 +0100

    Revert "B: eliminar funció resta"
    This reverts commit b19ed57eebde7bd055cc2917ae36eb9c34723990.

--- calc.py
+++ calc.py
@@ -1,11 +1,5 @@
 def suma(a, b):
    return a + b
-<<<<<<< HEAD
-
-def resta(a, b):
-   return a - b
 
 def multiplica(a, b):
    return a * b
-=======
->>>>>>> parent of b19ed57 (B: afegir funció resta)
